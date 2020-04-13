42b702436f6597eea5ceaaa805ae45334f4de239
Andrew Carter <andrewcarter1992@gmail.com>
2020-04-13 12:54:31 +0100

    Added websocket test server

---
+++ README.md
@@ -0,0 +1,15 @@
+# WebSocket Test Server
+
+This server is a simple docker container that was designed to test applications that interact with websockets.
+
+Every message received from a connected client is broadcast to all other connected clients (but not echoed back).
+
+This makes it simple to test the interaction of an application with an API that is provided using websockets.
+
+As this is designed to be used in a test suite, it is not suitable for production use.
+
+## Usage
+
+```sh
+docker run --rm -p 5000:5000 -e PORT=5000 andrewcarteruk/websocket-test-server
+```
---
+++ main.py
@@ -0,0 +1,59 @@
+import asyncio
+import websockets
+import os
+
+CONNECTED_CLIENTS = set()
+
+
+def escape(message):
+    replacements = {
+        '\r': '\\r',
+        '\n': '\\n',
+        '\t': '\\t',
+    }
+
+    for find, replace in replacements.items():
+        message = message.replace(find, replace)
+
+    return message
+
+
+async def handler(websocket, path):
+    CONNECTED_CLIENTS.add(websocket)
+
+    address = {
+        'host': websocket.remote_address[0],
+        'port': websocket.remote_address[1],
+    }
+
+    try:
+        print('Client connected ({host}:{port})'.format(**address))
+
+        async for message in websocket:
+            print('Client message received ({host}:{port}): {message}'.format(**address, message=escape(message)))
+
+            other_clients = filter(lambda client: client != websocket, CONNECTED_CLIENTS)
+
+            await asyncio.gather(
+                *[websocket.send(message) for websocket in other_clients],
+                return_exceptions=False,
+            )
+    except websockets.ConnectionClosedError:
+        pass
+    finally:
+        print('Client disconnected ({host}:{port})'.format(**address))
+        CONNECTED_CLIENTS.remove(websocket)
+
+
+def main():
+    port = int(os.environ['PORT'])
+
+    event_loop = asyncio.get_event_loop()
+
+    event_loop.run_until_complete(websockets.serve(handler, 'localhost', port))
+
+    event_loop.run_forever()
+
+
+if __name__ == '__main__':
+    main()
